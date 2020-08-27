de92e55e241c96ef72dfbddbee8e323e0a0dafff
xuexixuexiqq <273894953@qq.com>
2020-08-27 15:08:07 +0800

    添加文件读写代码

--- Src/main.c
+++ Src/main.c
@@ -47,7 +47,13 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+FATFS fs;
+FATFS *pfs;
+FIL fil;
+FRESULT fres;
+DWORD fre_clust;
+uint32_t totalSpace, freeSpace;
+char buffer[100];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -93,7 +99,56 @@ int main(void)
   MX_FATFS_Init();
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
-
+  
+  /* Wait for SD module reset */
+  HAL_Delay(500);
+  
+  /* Mount SD Card */
+  if(f_mount(&fs, "", 0) != FR_OK)
+    Error_Handler();
+  
+  /* Open file to write */
+  if(f_open(&fil, "first.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) != FR_OK)
+    Error_Handler();
+  
+  /* Check freeSpace space */
+  if(f_getfree("", &fre_clust, &pfs) != FR_OK)
+    Error_Handler();
+  
+  totalSpace = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
+  freeSpace = (uint32_t)(fre_clust * pfs->csize * 0.5);
+  
+  /* free space is less than 1kb */
+  if(freeSpace < 1)
+    Error_Handler();
+  
+  /* Writing text */
+  f_puts("STM32 SD Card I/O Example via SPI\n", &fil);
+  f_puts("Save the world!!!", &fil);
+  
+  /* Close file */
+  if(f_close(&fil) != FR_OK)
+    Error_Handler();
+  
+  /* Open file to read */
+  if(f_open(&fil, "first.txt", FA_READ) != FR_OK)
+    Error_Handler();
+  
+  while(f_gets(buffer, sizeof(buffer), &fil))
+  {
+    /* SWV output */
+    printf("%s", buffer);
+    fflush(stdout);
+  }
+  
+  /* Close file */
+  if(f_close(&fil) != FR_OK)
+    Error_Handler();
+  
+  /* Unmount SDCARD */
+  if(f_mount(NULL, "", 1) != FR_OK)
+    Error_Handler();
+  
   /* USER CODE END 2 */
 
   /* Infinite loop */
